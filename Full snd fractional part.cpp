052a4bdbc84c925b40a70423fe331235f17464da
huseyn-hesenov <hesenovhuseyn444@gmail.com>
2021-02-04 00:59:54 +0400

    code

---
+++ Full snd fractional part.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+	
+	cout<<"enter number: ";
+	double number;
+	cin>>number;
+	int fullPart=number;
+	cout<<"full part: "<<fullPart<<"\n";
+	double fractionalPart=number-fullPart;
+	cout<<"fractional part: "<<fractionalPart;
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
